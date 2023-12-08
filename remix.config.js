004cf8e466bd8f1cfff20063465f24cccf88501c
Kenta Kudo <yd37574@gmail.com>
2023-12-09 07:32:12 +0900

    feat: introduce remark / rehype plugins

--- remix.config.js
+++ remix.config.js
@@ -5,4 +5,17 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+
+  // MDX plugins
+  mdx: async (filename) => {
+    const [rehypeHighlight, remarkToc] = await Promise.all([
+      import("rehype-highlight").then((mod) => mod.default),
+      import("remark-toc").then((mod) => mod.default),
+    ]);
+
+    return {
+      rehypePlugins: [rehypeHighlight],
+      remarkPlugins: [remarkToc],
+    };
+  },
 };
