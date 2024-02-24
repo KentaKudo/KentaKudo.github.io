ed7abb5c8126c50b9ee8f7f8c91658abf4edf759
Kenta Kudo <yd37574@gmail.com>
2024-02-24 09:51:14 +0900

    feat: header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,27 @@
+import { Link } from "@remix-run/react";
 import { FC } from "react";
 
 export const Header: FC = () => {
   return (
     <header>
-      <p className="font-bold text-xl">Kenta Kudo</p>
+      <div className="h-20 mx-auto w-full max-w-screen-lg flex items-center justify-between">
+        <Link to="/" className="font-bold text-xl">
+          Kenta Kudo
+        </Link>
+        <nav>
+          <ul className="flex gap-4 font-bold">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
