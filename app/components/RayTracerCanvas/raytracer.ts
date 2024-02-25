a2e21968db18809eeacc190a0f8f062058269255
Kenta Kudo <yd37574@gmail.com>
2024-02-25 17:07:22 +0900

    fix: raytracer

--- app/components/RayTracerCanvas/raytracer.ts
+++ app/components/RayTracerCanvas/raytracer.ts
@@ -1,7 +1,5 @@
 //@ts-expect-error: TODO: check wasm-raytracer npm package settings
-// eslint-disable-next-line import/no-unresolved
 import init, { render as wasmRender } from "wasm-raytracer";
-// eslint-disable-next-line import/no-unresolved
 import wasm from "wasm-raytracer/binary.wasm?init";
 
 export const render = async (width: number, height: number) => {
