f79827f2ad6ea15f4900902c5fcb53e8d65d5ea7
Kenta Kudo <yd37574@gmail.com>
2024-09-05 09:39:33 +0900

    fix: stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,10 @@ export default function Home() {
         alt="Themes River"
         className="-z-10 w-full h-screen absolute top-0 right-0 bottom-0 left-0 object-center object-cover"
       />
-      <canvas id="webgl" className="absolute top-0 right-0 bottom-0 left-0" />
+      <canvas
+        id="webgl"
+        className="absolute top-0 right-0 bottom-0 left-0 z-[-10]"
+      />
     </>
   );
 }
